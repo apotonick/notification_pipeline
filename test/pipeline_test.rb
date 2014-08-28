d948b284e9f157238d314369b050c2468456f4cd
Nick Sutterer <apotonick@gmail.com>
2014-08-28 11:36:06 +1000

    channels now timestamp incoming messages.

--- test/pipeline_test.rb
+++ test/pipeline_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 require 'ostruct'
 module NotificationPipeline
+  # This can actually be kept in-memory in a daemon process.
   class Broadcast
     def initialize
       @channels = {}
@@ -17,8 +18,12 @@ module NotificationPipeline
     end
   end
 
+  # A Channel is a list of messages, usually "notifications" for a particular group, thread or object.
+  # Each message represents an event subscribers want to know about, like "Garrett liked photo XyZ!".
   class Channel < Array
     def <<(hash)
+      hash = hash.merge(created_at: timestamp)
+
       persist!(hash)
     end
 
@@ -38,6 +43,10 @@ module NotificationPipeline
       # Redis slice
       # where channel_id=1 and index>=2
     end
+
+    def timestamp
+      Time.now
+    end
   end
 
   class Notification < OpenStruct
@@ -48,6 +57,14 @@ end
 class PipelineTest < MiniTest::Spec
   Notification = NotificationPipeline::Notification
 
+  module Now
+    NOW = Time.now
+
+    def timestamp
+      NOW
+    end
+  end
+
   subject { NotificationPipeline::Broadcast.new }
 
   # non-existent channel.
@@ -55,11 +72,13 @@ class PipelineTest < MiniTest::Spec
 
   # push and read.
   it do
+    subject["new-songs"].extend(Now)
+
     subject["new-songs"] << {message: "Drones"}
     subject["new-songs"] << {message: "Them And Us"}
 
-    subject["new-songs", 0].must_equal [{message: "Drones"}, {message: "Them And Us"}] # next_i => 1
-    subject["new-songs", 1].must_equal [{message: "Them And Us"}] # next_i => 1
+    subject["new-songs", 0].must_equal [{message: "Drones", created_at: Now::NOW}, {message: "Them And Us", created_at: Now::NOW}] # next_i => 1
+    subject["new-songs", 1].must_equal [{message: "Them And Us", created_at: Now::NOW}] # next_i => 1
 
     # subscriber has to remember next index. or:
     #subject["new-songs", subscriber] # Broadcast remembers last i.
@@ -69,6 +88,6 @@ class PipelineTest < MiniTest::Spec
     # Notification#read!
 
     # Stream is "notifications" table or a Redis array per subscriber.
-
+    NotificationPipeline::Stream.new("new-songs" => subject["new-songs", 0])
   end
 end
